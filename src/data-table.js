0f6be017312ad570a41910da75872f3aaa3809a8
Mike Wilcox <anm9tr@gmail.com>
2017-10-09 13:15:33 -0500

    header

--- src/data-table.js
+++ src/data-table.js
@@ -1,17 +1,86 @@
 const BaseComponent = require('@clubajax/base-component');
 const dom = require('@clubajax/dom');
 
+const props = ['data'];
+const bools = [];
+
 class DataTable extends BaseComponent {
+
+	static get observedAttributes () {
+		return [...props, ...bools];
+	}
+
+	get props () {
+		return props;
+	}
+
+	get bools () {
+		return bools;
+	}
+
 	constructor () {
 		super();
-		console.log('DATA TABLE');
+	}
+
+	onData (value) {
+		console.log('DATA', value);
+		clearTimeout(this.noDataTimer);
+		onDomReady(this, () => {
+			this.render();
+		});
 	}
 
 	domReady () {
-		dom('div', {html: 'yay, table'}, this);
+		this.noDataTimer = setTimeout(() => {
+			console.warn('No data');
+		}, 1000);
+	}
+
+	render () {
+		this.fire('pre-render');
+		this.renderTemplate();
+		this.renderHeader(getColumns(this.data));
+	}
+
+	// is overwritten by scrollable
+	renderTemplate () {
+		if(this.table){
+			return;
+		}
+		this.table = dom('table', { tabindex:'1' }, this);
+		this.thead = dom('thead', {}, this.table);
+		this.tbody = dom('tbody', {}, this.table);
+	}
+
+	renderHeader (columns) {
+		dom.clean(this.thead, true);
+		const tr = dom('tr', {}, this.thead);
+
+		columns.forEach((col) => {
+			const key = col.key || col;
+			const label = col.label === undefined ? col : col.label;
+			const css = col.css || col.className || '';
+			dom('th', {
+				html: '<span>' + label + '</span>',
+				css: css,
+				'data-field': key
+			}, tr);
+		});
+		this.fire('render-header', {thead: this.thead});
 	}
 }
 
+function getColumns (data) {
+	if(Array.isArray(data.columns)) {
+		return data.columns;
+	}
+	return Object.keys(data.columns).map((key) => {
+		return {
+			key,
+			label: data.columns[key]
+		};
+	});
+}
 customElements.define('data-table', DataTable);
 
 module.exports = DataTable;
